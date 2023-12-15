e4fc96f4680766788f76504c08f4d3281df8834d
Alex <tarkovskiy.alexandr@protonmail.com>
2023-12-15 17:45:15 +0200

    update download script

--- scripts/update-list.js
+++ scripts/update-list.js
@@ -1,7 +1,8 @@
 const axios = require('axios').default;
-const { readFileSync, writeFileSync, mkdirSync, existsSync } = require('fs');
+const { readFileSync, writeFileSync, mkdirSync, existsSync, writeFile } = require('fs');
 const { isAddress, getAddress } = require('viem');
 const { join } = require('path');
+const { ChainId } = require('@real-wagmi/sdk');
 
 const lists = [
     "https://gateway.ipfs.io/ipns/tokens.uniswap.org",
@@ -20,6 +21,7 @@ const downloadImage = false;
 
 async function bootstrap(){
     const defaultList = JSON.parse(readFileSync(join(__dirname, '../tokenlist.json')));
+    const supportedChains = Object.values(ChainId).filter((chain) => !isNaN(Number(chain))).map((chain) => Number(chain));
     let count = 0;
 
     for(const url of lists){
@@ -28,24 +30,26 @@ async function bootstrap(){
             if(data && Array.isArray(data.tokens)){
                 for(const token of data.tokens){
                     if(isAddress(token.address) && !isNaN(token.chainId) && token.logoURI){
-                        const chainFolderExist = existsSync(join(__dirname, `../logos/${token.chainId}`));
-                        if(!chainFolderExist){
-                            mkdirSync(join(__dirname, `../logos/${token.chainId}`));
-                        }
+                        if(!supportedChains.includes(token.chainId)) continue;
+
                         const incomingTokenAddress = getAddress(token.address);
                         const isExist = defaultList.tokens.find(({ chainId, address }) => chainId === token.chainId && address === incomingTokenAddress);
                         if(!isExist){
+                            const chainFolderExist = existsSync(join(__dirname, `../logos/${token.chainId}`));
+                            if(!chainFolderExist){
+                                mkdirSync(join(__dirname, `../logos/${token.chainId}`));
+                            }
                             console.log(`new token incoming ${token.chainId} | ${incomingTokenAddress} | ${token.symbol}`);
                             if(downloadImage){
                                 mkdirSync(join(__dirname, `../logos/${token.chainId}/${incomingTokenAddress}/`));
                                 const response = await axios.get(token.logoURI, { responseType: "arraybuffer" });
-                                writeFileSync(join(__dirname, `../logos/${token.chainId}/${incomingTokenAddress}/logo.png`), response.data);
+                                writeFile(join(__dirname, `../logos/${token.chainId}/${incomingTokenAddress}/logo.png`), response.data);
                             }
 
                             defaultList.tokens.push({
                                 ...token,
                                 address: incomingTokenAddress,
-                                logoURI: `https://raw.githubusercontent.com/RealWagmi/tokenlists/main/logos/${token.chainId}/${incomingTokenAddress}/logo.png`
+                                logoURI: downloadImage ? `https://raw.githubusercontent.com/RealWagmi/tokenlists/main/logos/${token.chainId}/${incomingTokenAddress}/logo.png`: token.logoURI
                             });
                         }
                     }
